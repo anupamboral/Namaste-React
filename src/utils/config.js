f8ebd480450d4e7ed0070625bb956186dd36965e
anupamboral <anupamboral6889@gmail.com>
2024-06-07 23:18:33 +0530

    Lesson - 6 - Let's Explore the world - updated notes

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,5 @@
 //*both are ways of doing named exports
+//* the below cdn url is used for restaurant card images(go to restaurant card.js to see it's usage , this cdn link is basically same for all the images but only a id(cloudinaryImageId ) is different for every image , so as the cdn link(first part of the link )is same for every image that's why we kept the cdn lik here in the config file, and then we import it in the restaurantCard.js file and then we concat the cdn link and the cloudinaryImageId , which helps us to get images for all of the restaurant cards.
 export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
