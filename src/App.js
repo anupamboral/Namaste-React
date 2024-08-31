1aa95e00e37b21ef9995105242b750d2e55435ed
anupamboral <anupamboral6889@gmail.com>
2024-09-01 04:02:33 +0530

    notes updated

--- src/App.js
+++ src/App.js
@@ -18,10 +18,10 @@ import Shimmer from "./components/Shimmer";
 
 import UserContext from "./utils/UserContext.js";
 
-//! Lesson - 11 - data is new oil
+//! Lesson - 12 - let's build our store
 //*
 //*My notes
-//* First of all many companies start using deducts from day one even without understanding they need it or not and some developers even think that redux is part of react. but no , react and redux are two different libraries . redux is state management or data management tool , very popular today.
+//* First of all many companies start using deducts from day one even without understanding they need it or not and some developers even think that redux is part of react. but no , react and redux are two different libraries . redux is state management or data management tool , very poplar today.
 
 //* about redux website:- If we go to the website of redux then after opening their website we will see the description written that Redux is a predictable state container for js apps so basically a state management library for js apps and we can also understand that Redux is not only specific to react but it can be also used with other js libraries and frameworks now redux Has two other libraries first one is react-react and the second one is Redux toolkit.There is a normal version of Redux which is the older version and it is also called vanilla redux.But we're not gonna use the older vanilla redux Because with time redux simplified things and they created Redux Toolkit which is very easy to use then the older version also the react redux Which is like a bridge between react and redux. So we are gonna use react redux and redux toolkit in our project so we can understand the modern way of using Redux rather than the older version.
 
